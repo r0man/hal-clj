1fa5e4ee12e51fe3245a0ba9bdf9874a204c2cf2
Roman Scherer <roman.scherer@burningswell.com>
2014-01-19 00:55:14 +0100

    Initial commit.

---
+++ src/hal/core.clj
@@ -0,0 +1,40 @@
+(ns hal.core
+  (:require [clojure.string :refer [blank?]]))
+
+(defn embedded
+  ([res]
+     (:_embedded res))
+  ([res k]
+     (get (embedded res) k)))
+
+(defn links
+  ([res]
+     (:_links res))
+  ([res k]
+     (get (links res) k)))
+
+(defn href [res k]
+  (:href (get (links res) k)))
+
+(defn self-href [res]
+  (-> res :_links :self :href))
+
+(defn with-hrefs
+  [res & kvs]
+  (reduce
+   (fn [res [k href]]
+     (if-not (blank? href)
+       (assoc-in res [:_links k :href] href)
+       res))
+   res (partition 2 kvs)))
+
+(defn with-embedded
+  [res & kvs]
+  (reduce
+   (fn [res [k embedded]]
+     (if embedded
+       (-> (assoc-in res [:_embedded k] embedded)
+           (assoc-href k (-> embedded :_links :self :href))
+           identity)
+       res))
+   res (partition 2 kvs)))
---
+++ test/hal/core_test.clj
@@ -0,0 +1,38 @@
+(ns hal.core-test
+  (:require [clojure.test :refer :all]
+            [hal.core :refer :all]))
+
+(def europe
+  {:name "Europe"
+   :_links {:self {:href "http://example.com/continents/1"}}})
+
+(def germany
+  {:name "Germany"
+   :_links {:self {:href "http://example.com/countries/1"}}})
+
+(deftest test-with-hrefs
+  (is (= (with-hrefs {}
+           :continent nil)
+         {}))
+  (is (= (with-hrefs {}
+           :continent "http://example.com/continents/1")
+         {:_links {:continent {:href "http://example.com/continents/1"}}}))
+  (is (= (with-hrefs {}
+           :continent "http://example.com/continents/1"
+           :country "http://example.com/country/1")
+         {:_links {:continent {:href "http://example.com/continents/1"}
+                   :country {:href "http://example.com/country/1"}}})))
+
+(deftest test-with-embedded
+  (is (= (with-embedded {}
+           :continent nil)
+         {}))
+  (is (= (with-embedded {}
+           :continent europe)
+         {:_links {:continent {:href "http://example.com/continents/1"}}
+          :_embedded {:continent europe}})))
+
+(deftest test-href
+  (is (nil? (href europe nil)))
+  (is (nil? (href europe :not-existing)))
+  (is (= (href europe :self) (-> europe :_links :self :href))))

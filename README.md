6b5e7dac05720a0cb2e3c8f4f40f92e5655cc7e9
Roman Scherer <roman.scherer@burningswell.com>
2014-01-19 10:51:24 +0100

    Build.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hal
+# HAL-CLJ [![Build Status](https://travis-ci.org/r0man/hal-clj.png)](https://travis-ci.org/r0man/hal-clj)
 
 A Clojure library designed to ... well, that part is up to you.
 
@@ -8,7 +8,7 @@ FIXME
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Roman Scherer
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hal-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure library for the Hypertext Application Language"
+  :url "https://github.com/r0man/hal-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])

7be4a13a79ef88aeb39caf6f119df2b5b7cdf4a1
Roman Scherer <roman.scherer@burningswell.com>
2014-01-24 21:39:29 +0100

    Run tests on node as well.

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   :lein-release {:deploy-via :clojars}
   :cljsbuild {:builds []}
   :dependencies [[noencore "0.1.11"]
-                 [request-clj "0.2.0"]]
+                 [request-clj "0.2.1-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/clojurescript "0.0-2138"]
                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
                              [com.cemerick/austin "0.1.3"]
-                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [com.cemerick/clojurescript.test "0.2.2-SNAPSHOT"]
                              [lein-cljsbuild "1.0.1"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
                    :cljx {:builds [{:source-paths ["src"]
@@ -33,7 +33,8 @@
                                                     :output-dir "target/test"
                                                     :optimizations :advanced
                                                     :pretty-print true}}]
-                               :test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}}
+                               :test-commands {"node" ["node" :node-runner "target/testable.js"]
+                                               "phantom" ["phantomjs" :runner "target/testable.js"]}}
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :resource-paths ["test-resources"]
                    :source-paths ["target/classes"]

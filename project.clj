c72798961437ad28bf8cedb321576ab40a51d041
Roman Scherer <roman.scherer@burningswell.com>
2014-05-24 16:13:11 +0200

    Bump version from 0.1.7 to 0.1.8-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hal-clj "0.1.7"
+(defproject hal-clj "0.1.8-SNAPSHOT"
   :description "Clojure library for the Hypertext Application Language"
   :url "https://github.com/r0man/hal-clj"
   :license {:name "Eclipse Public License"

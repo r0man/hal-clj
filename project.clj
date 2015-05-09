6654c44eec7d50705b363cc33f1a7131655a980c
Roman Scherer <roman@burningswell.com>
2015-05-09 16:02:00 +0200

    Version 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hal-clj "0.1.12-SNAPSHOT"
+(defproject hal-clj "0.1.12"
   :description "Clojure library for the Hypertext Application Language"
   :url "https://github.com/r0man/hal-clj"
   :author "r0man"
